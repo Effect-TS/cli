6fe98f97f813d9b797822391c1703ff5483c72b4
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-03-19 08:59:50 -0400

    initial release

--- packages/cli/src/Options/index.ts
+++
@@ -1,8 +0,0 @@
-// ets_tracing: off
-
-// codegen:start { preset: barrel, include: ./*.ts }
-export * from "./definition.js"
-export * from "./dsl.js"
-export * from "./instances.js"
-export * from "./operations.js"
-// codegen:end
