94b219ed3985891dfed82e42ebc4a26a429e8169
Tim <hello@timsmart.co>
2023-10-11 09:46:27 +1300

    use preconstruct for builds (#338)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,11 @@
 /// <reference types="vitest" />
 
-import babel from "@vitejs/plugin-react"
-import path from "path"
+import * as path from "path"
 import { defineConfig } from "vite"
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const babelConfig = require("./.babel.mjs.json")
-
 export default defineConfig({
-  plugins: [babel({ babel: babelConfig })],
   test: {
-    include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    include: ["./test/**/*.ts"],
     exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
     globals: true
   },
